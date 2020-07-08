6de62f48b8da6db8e6e7f743b87720254799ccfd
Piyush Mishra <piyush_mishra31@outlook.com>
2020-07-08 15:54:20 +0530

    working lambda functions

--- originresponselambda/index.js
+++ originresponselambda/index.js
@@ -1,165 +1,145 @@
-const path = require("path");
+const querystring = require("querystring");
+
 const AWS = require("aws-sdk");
 const S3 = new AWS.S3({
   signatureVersion: "v4",
 });
 const Sharp = require("sharp");
-const querystring = require("querystring");
+
+// set the S3 and API GW endpoints
 const BUCKET = "html-cms";
+
 exports.handler = async (event, context, callback) => {
-  const { request, response } = event.Records[0].cf;
-  const { uri } = request;
-  const params = querystring.parse(request.querystring);
-  const headers = response.headers;
+  let response = event.Records[0].cf.response;
+  console.log("Response status code :%s", response.status);
 
-  // if there is no dimension attribute, just pass the response
-  if (!params.project) {
-    console.log("project param not found");
-    callback(null, response);
-    return;
-  } else {
-    if (!params.d) {
-      if (path.extname(uri) === ".webp") {
-        if (response.status == 404) {
-          const format =
-            request.headers["original-resource-type"] &&
-            request.headers["original-resource-type"][0]
-              ? request.headers["original-resource-type"][0].value.replace(
-                  "image/",
-                  ""
-                )
-              : null;
+  //check if image is not present
+  if (response.status == 404) {
+    let request = event.Records[0].cf.request;
+    const originalFormat =
+      request.headers["original-resource-type"] &&
+      request.headers["original-resource-type"][0]
+        ? request.headers["original-resource-type"][0].value.replace(
+            "image/",
+            ""
+          )
+        : null;
+    let params = querystring.parse(request.querystring);
 
-          const key = params.project + uri;
-          const s3key = key.replace(".webp", `.${format}`).replace("/webp", "");
-          try {
-            const bucketResource = await S3.getObject({
-              Bucket: BUCKET,
-              Key: s3key,
-            }).promise();
-            let sharpImageBuffer = await Sharp(bucketResource.Body);
-            if (params.quality)
-              sharpImageBuffer = await sharpImageBuffer.webp({
-                quality: +parseInt(params.quality),
-              });
-            else
-              sharpImageBuffer = await sharpImageBuffer.webp({
-                quality: +75,
-              });
-            sharpImageBuffer = await sharpImageBuffer.toBuffer();
+    // if there is no dimension attribute, just pass the response
+    if (!params.project) {
+      callback(null, response);
+      return;
+    }
 
-            await S3.putObject({
-              ACL: "public-read",
-              ContentType: "image/webp",
-              ContentLength: sharpImageBuffer.length,
-              CacheControl: "max-age=31536000",
-              Body: sharpImageBuffer,
-              Bucket: BUCKET,
-              Key: key,
-              Expires: new Date().setDate(new Date().getDate() + 2),
-            }).promise();
+    // read the required path. Ex: uri /images/resized/webp/image-100x100.webp
+    let path = request.uri;
 
-            response.status = 200;
-            response.body = sharpImageBuffer.toString("base64");
-            response.bodyEncoding = "base64";
-            response.headers["content-type"] = [
-              { key: "Content-Type", value: "image/webp" },
-            ];
-          } catch (error) {
-            console.log(error);
-            callback(null, response);
-          }
-        } else {
-          headers["content-type"] = [
-            {
-              value: "image/webp",
-              key: "Content-Type",
-            },
-          ];
-        }
+    // read the S3 key from the path variable.
+    // Ex: path variable /images/resized/webp/image-100x100.webp
+    let key = path.substring(1);
+
+    // parse the prefix, width, height and image name
+    // Ex: key=images/200x200/webp/image.jpg
+    let prefix, originalKey, match, requiredFormat, imageName;
+
+    try {
+      match = key.match(/(.*)\/(resized)\/(.*)\/(.*)/);
+      prefix = match[1];
+      const dimensionMatch = params.d.split("x");
+      // set the width and height parameters
+      let width = dimensionMatch[0];
+      let height = dimensionMatch[1];
+      // correction for jpg required for 'Sharp'
+      requiredFormat = match[3] == "jpg" ? "jpeg" : match[3];
+      imageName = match[4].replace(`-${width}x${height}`, "");
+      originalKey =
+        params.project +
+        "/" +
+        prefix +
+        "/" +
+        imageName.replace(`.${requiredFormat}`, `.${originalFormat}`);
+    } catch (err) {
+      // no resized folder exist for image..
+      console.log("no resized folder present..", err);
+      match = key.match(/(.*)\/(.*)\/(.*)/);
+      prefix = match[1];
+      // correction for jpg required for 'Sharp'
+      requiredFormat = match[2] == "jpg" ? "jpeg" : match[2];
+      imageName = match[3];
+      originalKey =
+        params.project +
+        "/" +
+        prefix +
+        "/" +
+        imageName.replace(`.${requiredFormat}`, `.${originalFormat}`);
+    }
+
+    console.log(originalKey);
+
+    try {
+      // get the source image file
+      let data = await S3.getObject({
+        Bucket: BUCKET,
+        Key: originalKey,
+      }).promise();
+
+      // perform the resize operation
+      let sharpifiedImage = Sharp(data.Body);
+
+      if (params.d) {
+        const dimensionMatch = params.d.split("x");
+        // set the width and height parameters
+        let width = parseInt(dimensionMatch[0]);
+        let height = parseInt(dimensionMatch[1]);
+        sharpifiedImage = sharpifiedImage.resize(width, height);
       }
-    } else {
-      // read the required path. Ex: uri /images/100x100/webp/image.jpg
-      let path = request.uri;
-      const format =
-        request.headers["original-resource-type"] &&
-        request.headers["original-resource-type"][0]
-          ? request.headers["original-resource-type"][0].value.replace(
-              "image/",
-              ""
-            )
-          : null;
-      // read the S3 key from the path variable.
-      // Ex: path variable /images/100x100/webp/image.jpg
-      let key = path.substring(1);
-
-      // parse the prefix, width, height and image name
-      // Ex: key=images/200x200/webp/image.jpg
-      let prefix, originalKey, match, width, height, requiredFormat, imageName;
-
-      try {
-        match = key.match(/(.*)\/(\d+)x(\d+)\/(.*)\/(.*)/);
-        prefix = match[1];
-        width = parseInt(match[2], 10);
-        height = parseInt(match[3], 10);
-
-        // correction for jpg required for 'Sharp'
-        requiredFormat = match[4] == "jpg" ? "jpeg" : match[4];
-        imageName = match[5];
-        originalKey =
-          params.project +
-          "/" +
-          prefix +
-          "/" +
-          imageName.replace(`.${requiredFormat}`, `.${format}`);
-      } catch (err) {
-        console.log(err);
-        callback(null, response);
-      }
-      try {
-        // get the source image file
-        let data = await S3.getObject({
-          Bucket: BUCKET,
-          Key: originalKey,
-        }).promise();
-        // perform the resize operation
-        let sharpedImage = await Sharp(data.Body).resize(width, height);
-        if (path.extname(uri) === ".webp") {
-          if (params.quality) {
-            sharpedImage = await sharpedImage.webp({
+      if (params.quality) {
+        switch (requiredFormat) {
+          case "jpeg":
+            sharpifiedImage = sharpifiedImage.jpeg({
               quality: +parseInt(params.quality),
             });
-          } else sharpedImage = await sharpedImage.webp({ quality: +75 });
-        } else sharpedImage = await sharpedImage.toFormat(requiredFormat);
+            break;
+          case "webp":
+            sharpifiedImage = sharpifiedImage.webp({
+              quality: +parseInt(params.quality),
+            });
+            break;
+          default:
+            sharpifiedImage = sharpifiedImage.toFormat(requiredFormat);
+        }
+      } else sharpifiedImage = sharpifiedImage.toFormat(requiredFormat);
 
-        sharpedImage = await sharpedImage.toBuffer();
-        // save the resized object to S3 bucket with appropriate object key.
-        await S3.putObject({
-          Body: sharpedImage,
-          Bucket: BUCKET,
-          ContentType: "image/" + requiredFormat,
-          Key: params.project + "/" + key,
-          ACL: "public-read",
-          ContentLength: sharpedImage.length,
-          CacheControl: "max-age=31536000",
-          Expires: new Date().setDate(new Date().getDate() + 2),
-        }).promise();
-        // even if there is exception in saving the object we send back the generated
-        // image back to viewer below
-
-        // generate a binary response with resized image
-        response.status = 200;
-        response.body = sharpedImage.toString("base64");
-        response.bodyEncoding = "base64";
-        response.headers["content-type"] = [
-          { key: "Content-Type", value: "image/" + requiredFormat },
-        ];
-        callback(null, response);
-      } catch (err) {
-        console.log("Exception while reading source image :%j", err);
-        callback(null, response);
-      }
+      let buffer = await sharpifiedImage.toFormat(requiredFormat).toBuffer();
+      console.log(key);
+      // save the resized object to S3 bucket with appropriate object key.
+      let insertResp = await S3.putObject({
+        Body: buffer,
+        Bucket: BUCKET,
+        ContentType: "image/" + requiredFormat,
+        CacheControl: "max-age=31536000",
+        Key: params.project + "/" + key,
+        ACL: "public-read",
+        ContentLength: buffer.length,
+        Expires: new Date().setDate(new Date().getDate() + 2),
+      }).promise();
+      console.log(insertResp);
+      // generate a binary response with resized image
+      response.status = 200;
+      response.body = buffer.toString("base64");
+      response.bodyEncoding = "base64";
+      response.headers["content-type"] = [
+        { key: "Content-Type", value: "image/" + requiredFormat },
+      ];
+      callback(null, response);
+    } catch (err) {
+      console.log(err);
+      callback(null, response);
     }
+  } // end of if block checking response statusCode
+  else {
+    // allow the response to pass through
+    callback(null, response);
   }
-  callback(null, response);
 };
--- viewerrequestlambda/index.js
+++ viewerrequestlambda/index.js
@@ -1,151 +1,104 @@
-const userAgent = require("useragent");
-const path = require("path");
 const querystring = require("querystring");
+const path = require("path");
+const userAgent = require("useragent");
 
 const variables = {
-  allowedDimension: [
-    { w: 100, h: 100 },
-    { w: 200, h: 200 },
-    { w: 300, h: 300 },
-    { w: 400, h: 400 },
-  ],
-  defaultDimension: { w: 200, h: 200 },
-  variance: 20,
   webpExtension: "webp",
 };
 
-const browsersToInclude = [
-  { browser: "Chrome", version: 23 },
-  { browser: "Opera", version: 15 },
-  { browser: "Android", version: 53 },
-  { browser: "Chrome Mobile", version: 55 },
-  { browser: "Opera Mobile", version: 37 },
-  { browser: "UC Browser", version: 11 },
-  { browser: "Samsung Internet", version: 4 },
-  { browser: "Safari", version: 14 },
-];
-
-exports.handler = async (event, context, callback) => {
+exports.handler = (event, context, callback) => {
   const request = event.Records[0].cf.request;
   const headers = request.headers;
+
+  const userAgentString =
+    headers["user-agent"] && headers["user-agent"][0]
+      ? headers["user-agent"][0].value
+      : null;
+  const agent = userAgent.lookup(userAgentString);
+
+  const browsersToInclude = [
+    { browser: "Chrome", version: 23 },
+    { browser: "Opera", version: 15 },
+    { browser: "Android", version: 53 },
+    { browser: "Chrome Mobile", version: 55 },
+    { browser: "Opera Mobile", version: 37 },
+    { browser: "UC Browser", version: 11 },
+    { browser: "Samsung Internet", version: 4 },
+    { browser: "Safari", version: 14 },
+  ];
+
+  const supportingBrowser = browsersToInclude.find(
+    (browser) =>
+      browser.browser === agent.family && agent.major >= browser.version
+  );
+
   // parse the querystrings key-value pairs. In our case it would be d=100x100
   const params = querystring.parse(request.querystring);
+
+  // fetch the uri of original image
+  let fwdUri = request.uri;
+  if (supportingBrowser)
+    fwdUri = fwdUri.replace(`.${path.extname(request.uri)}`, ".webp");
 
   // if there is no dimension attribute, just pass the request
   if (!params.project) {
     callback(null, request);
     return;
-  } else {
-    const userAgentString =
-      headers["user-agent"] && headers["user-agent"][0]
-        ? headers["user-agent"][0].value
-        : null;
-    const agent = userAgent.lookup(userAgentString);
-
-    const supportingBrowser = browsersToInclude.find(
-      (browser) =>
-        browser.browser === agent.family && agent.major >= browser.version
-    );
-    if (!params.d) {
-      if (supportingBrowser) {
-        const fileFormat = path.extname(request.uri).replace(".", "");
-        request.headers["original-resource-type"] = [
-          {
-            key: "Original-Resource-Type",
-            value: `image/${fileFormat}`,
-          },
-        ];
-        const match = fwdUri.match(/(.*)\/(.*)\.(.*)/);
-        let prefix = match[1];
-
-        let imageName = match[2];
-        let url = [];
-
-        url.push(prefix);
-        url.push(variables.webpExtension);
-        url.push(imageName + "." + variables.webpExtension);
-        fwdUri = url.join("/");
-        request.uri = fwdUri;
-      }
-    } else {
-      // fetch the uri of original image
-      let fwdUri = request.uri;
-
-      // read the dimension parameter value = width x height and split it by 'x'
-      const dimensionMatch = params.d.split("x");
-
-      // set the width and height parameters
-      let width = dimensionMatch[0];
-
-      let height = dimensionMatch[1];
-
-      // parse the prefix, image name and extension from the uri.
-      // In our case /images/image.jpg
-
-      const match = fwdUri.match(/(.*)\/(.*)\.(.*)/);
-
-      let prefix = match[1];
-
-      let imageName = match[2];
-
-      let extension = match[3];
-
-      // define variable to be set to true if requested dimension is allowed.
-      let matchFound = false;
-
-      // calculate the acceptable variance. If image dimension is 105 and is within acceptable
-      // range, then in our case, the dimension would be corrected to 100.
-      let variancePercent = variables.variance / 100;
-
-      for (let dimension of variables.allowedDimension) {
-        let minWidth = dimension.w - dimension.w * variancePercent;
-
-        let maxWidth = dimension.w + dimension.w * variancePercent;
-
-        if (width >= minWidth && width <= maxWidth) {
-          width = dimension.w;
-
-          height = dimension.h;
-
-          matchFound = true;
-
-          break;
-        }
-      }
-      // if no match is found from allowed dimension with variance then set to default
-      //dimensions.
-      if (!matchFound) {
-        width = variables.defaultDimension.w;
-
-        height = variables.defaultDimension.h;
-      }
-
-      let url = [];
-
-      url.push(prefix);
-
-      url.push(width + "x" + height);
-
-      if (supportingBrowser) {
-        const fileFormat = path.extname(request.uri).replace(".", "");
-        request.headers["original-resource-type"] = [
-          {
-            key: "Original-Resource-Type",
-            value: `image/${fileFormat}`,
-          },
-        ];
-        url.push(variables.webpExtension);
-        url.push(imageName + "." + variables.webpExtension);
-      } else {
-        url.push(extension);
-        url.push(imageName + "." + extension);
-      }
-
-      fwdUri = url.join("/");
-
-      request.uri = fwdUri;
-    }
   }
 
-  return callback(null, request);
+  // parse the prefix, image name and extension from the uri.
+  // In our case /images/image.jpg
+
+  const match = fwdUri.match(/(.*)\/(.*)\.(.*)/);
+
+  let prefix = match[1]; // "images"
+  let imageName = match[2]; // "imagename"
+  let extension = match[3]; // "jpg"
+
+  // read the accept header to determine if webP is supported.
+  let accept = headers["accept"] ? headers["accept"][0].value : "";
+
+  let url = [];
+  // build the new uri to be forwarded upstream
+  url.push(prefix);
+
+  // check if dimension parameter is sent
+  if (params.d) {
+    url.push("resized");
+  }
+
+  // check support for webp
+  if (accept.includes(variables.webpExtension)) {
+    extension = variables.webpExtension;
+    url.push(variables.webpExtension);
+  } else {
+    url.push(extension);
+  }
+
+  // check if dimension parameter is sent
+  if (params.d) {
+    // read the dimension parameter value = width x height and split it by 'x'
+    const dimensionMatch = params.d.split("x");
+
+    // set the width and height parameters
+    let width = dimensionMatch[0];
+    let height = dimensionMatch[1];
+    url.push(imageName + "-" + width + "x" + height + "." + extension);
+  } else url.push(imageName + "." + extension);
+
+  fwdUri = url.join("/");
+
+  const fileFormat = path.extname(request.uri).replace(".", "");
+  request.headers["original-resource-type"] = [
+    {
+      key: "Original-Resource-Type",
+      value: `image/${fileFormat}`,
+    },
+  ];
+
+  console.log(fwdUri);
+
+  // final modified url is of format /images/200x200/webp/image.jpg
+  request.uri = fwdUri;
+  callback(null, request);
 };
